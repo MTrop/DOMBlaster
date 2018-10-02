1d24364fb0c684f5be4a40995d2be57ff341bc9f
mtrop <mtropiano@gmail.com>
2018-10-02 12:24:30 -0400

    Changed 'forms' a little

--- extensions/domblaster.forms.js
+++ extensions/domblaster.forms.js
@@ -30,6 +30,70 @@ DOMBlaster.extend('value', function(v) {
  * @param callback [optional] if provided, call this function with one argument: the data returned.
  * @return an object of the name/value pairings of the form fields, or the selection group if a callback was provided.
  */
+DOMBlaster.extendGroup('formState', function(callback) {
+	
+	if (callback && 'function' !== typeof(callback))
+		throw new Error("Callback function for formData must be a function!");
+	
+	let out = {};
+	let f = 0;
+	this.each(function()
+	{
+		let formData = {};
+		let dbForm = DOMBlaster(this);
+		let formId = this.getAttribute('id') || ('form' + (f++));
+
+		let FILLFUNC = function()
+		{
+			let memberName = this.getAttribute('name');
+			if (memberName)
+			{
+				if (!DOMBlaster.matches(this, ':disabled'))
+				{
+					let t = this.getAttribute('type');
+					let v = t === 'checkbox' || t === 'radio' ? DOMBlaster.matches(this, ':checked') : this.value;
+				
+					if ('object' === typeof(formData[memberName]))
+					{
+						formData[memberName].push(v);
+					}
+					else if (formData[memberName])
+					{
+						let arr = [formData[memberName], v];
+						formData[memberName] = arr;
+					}
+					else
+						formData[memberName] = v;
+				}
+			}
+		};
+
+		dbForm.select('input, textarea, select').each(FILLFUNC);
+		if (callback)
+			callback(formData);
+		else
+			out[formId] = formData;
+	});
+
+	let objkeys = Object.keys(out);
+	
+	if (callback)
+		return this;
+	else if (objkeys.length === 1)
+		return out[objkeys[0]];
+	else if (objkeys.length === 0)
+		return null;
+	else
+		return out;
+});
+
+/**
+ * Scrapes a form (each form in collection) for its values and returns an object of the name-value pairings of the form fields,
+ * or an object mapping (name/id for key).
+ * The 'id' attribute is used if 'name' is not provided. Unnamed form elements are not scraped.
+ * @param callback [optional] if provided, call this function with one argument: the data returned.
+ * @return an object of the name/value pairings of the form fields, or the selection group if a callback was provided.
+ */
 DOMBlaster.extendGroup('formData', function(callback) {
 	
 	if (callback && 'function' !== typeof(callback))
@@ -45,23 +109,30 @@ DOMBlaster.extendGroup('formData', function(callback) {
 
 		let FILLFUNC = function()
 		{
-			let dbt = DOMBlaster(this);
-			let memberName = this.getAttribute('name') || this.getAttribute('id');
+			let memberName = this.getAttribute('name');
 			if (memberName)
 			{
-				if (!dbt.matches(':disabled') && (this.getAttribute('type') !== 'checkbox' || dbt.matches(':checked')))
+				if (!DOMBlaster.matches(this, ':disabled'))
 				{
+					let t = this.getAttribute('type');
+					if (t === 'checkbox' && !DOMBlaster.matches(this, ':checked'))
+						return;
+					if (t === 'radio' && !DOMBlaster.matches(this, ':checked'))
+						return;
+					
+					let v = this.value;
+				
 					if ('object' === typeof(formData[memberName]))
 					{
-						formData[memberName].push(dbt.value());
+						formData[memberName].push(v);
 					}
 					else if (formData[memberName])
 					{
-						let arr = [formData[memberName], dbt.value()];
+						let arr = [formData[memberName], v];
 						formData[memberName] = arr;
 					}
 					else
-						formData[memberName] = dbt.value();
+						formData[memberName] = v;
 				}
 			}
 		};
